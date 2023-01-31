f9d7fa389b8a2ecca8cc4d3f671912ecd2f3f34f
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-01-31 22:07:28 +0530

    build: fetch service credentials from storage

--- config/firebase-admin.ts
+++ config/firebase-admin.ts
@@ -3,12 +3,21 @@ import { getFirestore } from 'firebase-admin/firestore';
 import {getStorage,} from 'firebase-admin/storage';
 import {getAuth} from 'firebase-admin/auth';
 import admin, { credential } from "firebase-admin"
-const serviceAccount = require('../firebase-admin.json');
+import fetch from "node-fetch"
+// const serviceAccount = require('../firebase-admin.json');
 
-if(!admin.apps.length && serviceAccount) {
+const credUrl = process.env.CREDENTIALS_PATH as string
+const credKey = process.env.CREDENTIALS_KEY as string
+
+const url = credUrl + "&token=" + encodeURIComponent(credKey)
+
+const serviceAccount2 = await fetch(url).then((res) => res.json())
+// console.log(serviceAccount2)
+
+if(!admin.apps.length && serviceAccount2) {
     
     initializeApp({
-        credential: credential.cert(serviceAccount),
+        credential: credential.cert(serviceAccount2),
     });
     
 }
