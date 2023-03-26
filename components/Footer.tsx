a897cd547925e6255b6a0375069b326c92561688
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-03-26 12:00:53 +0530

    favicons added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,68 +18,78 @@ const Footer: React.FC = () => {
             onClick={() => router.push("/")}
           />
           <span
-            className="text-3xl font-semibold tracking-wider xl:text-4xl cursor-pointer"
+            className="text-3xl font-semibold tracking-wider xl:text-4xl cursor-pointer "
             onClick={() => router.push("/")}
           >
             Activity Dekho
           </span>
         </span>
-        <p className="mx-auto w-max">© 2023 Activity Dekho</p>
+        <a
+          href="https://github.com/JayeshVP24/Activity-Dekho/blob/main/LICENSE"
+          className="mx-auto text-center block"
+        >
+          © 2023 Activity Dekho
+        </a>
       </div>
       <div>
-      <div className="flex flex-wrap underline gap-x-6 justify-center my-4 lg:my-6 ">
-        <a href="https://github.com/JayeshVP24/Activity-Dekho">Open Source</a>
-        <a href="https://github.com/JayeshVP24/Activity-Dekho/blob/main/CONTRIBUTING.md">
-          Contribute
-        </a>
-        <a href="https://github.com/JayeshVP24/Activity-Dekho/issues">
-          Report Bug
-        </a>
-        <a href="https://github.com/JayeshVP24/Activity-Dekho/issues">
-          Request Feature
-        </a>
-      </div>
-      <div className="text-center mt-4 lg:mt-6">
-        <p>Developed with 🤍 by yours truly 😊</p>
-        <a
-          href="https://twitter.com/jayeshvp24"
-          className="font-bold underline"
-        >
-          Jayesh Potlabattini
-        </a>
-        <span className="flex flex-wrap gap-4 lg:gap-8 justify-center mt-4 lg:mt-6">
-          <a href="https://twitter.com/jayeshvp24">
-            <Image
-              className=""
-              src="/twitter.svg"
-              width="50"
-              height="50"
-              alt="LinkedIn logo"
-            />
+        <div className="flex flex-wrap underline gap-x-6 justify-center my-4 lg:my-6 ">
+          <a href="https://github.com/JayeshVP24/Activity-Dekho">Open Source</a>
+          <a href="https://github.com/JayeshVP24/Activity-Dekho/blob/main/CONTRIBUTING.md">
+            Contribute
           </a>
-          <a href="https://www.linkedin.com/in/jayesh-potlabattini/">
-            <Image
-              className=""
-              src="/linkedin.svg"
-              width="50"
-              height="50"
-              alt="LinkedIn logo"
-            />
+          <a href="https://github.com/JayeshVP24/Activity-Dekho/issues">
+            Report Bug
           </a>
-          <a href="https://github.com/JayeshVP24/">
-            <Image
-              className=""
-              src="/github.svg"
-              width="50"
-              height="50"
-              alt="LinkedIn logo"
-            />
+          <a href="https://github.com/JayeshVP24/Activity-Dekho/issues">
+            Request Feature
           </a>
-          <a href="https://jayeshvp24.dev/">
-            <Image
-              className="" src="/www.svg" width="50" height="50" alt="LinkedIn logo" />
+        </div>
+        <div className="text-center mt-4 lg:mt-6">
+          <p>Developed with 🤍 by yours truly 😊</p>
+          <a
+            href="https://twitter.com/jayeshvp24"
+            className="font-bold underline"
+          >
+            Jayesh Potlabattini
           </a>
-        </span>
+          <span className="flex flex-wrap gap-4 lg:gap-8 justify-center mt-4 lg:mt-6">
+            <a href="https://twitter.com/jayeshvp24">
+              <Image
+                className=""
+                src="/twitter.svg"
+                width="50"
+                height="50"
+                alt="LinkedIn logo"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/jayesh-potlabattini/">
+              <Image
+                className=""
+                src="/linkedin.svg"
+                width="50"
+                height="50"
+                alt="LinkedIn logo"
+              />
+            </a>
+            <a href="https://github.com/JayeshVP24/">
+              <Image
+                className=""
+                src="/github.svg"
+                width="50"
+                height="50"
+                alt="LinkedIn logo"
+              />
+            </a>
+            <a href="https://jayeshvp24.dev/">
+              <Image
+                className=""
+                src="/www.svg"
+                width="50"
+                height="50"
+                alt="LinkedIn logo"
+              />
+            </a>
+          </span>
         </div>
       </div>
     </footer>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import GlobalStateProvider from "@/components/GlobalStateProvider";
 import "@/styles/globals.css";
+import Head from "next/head"
 import { Raleway } from "@next/font/google";
 import type { AppProps } from "next/app";
 const poppins = Raleway({
@@ -11,6 +12,29 @@ const poppins = Raleway({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${poppins.variable} font-sans`} >
+    <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#9f00a7" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <GlobalStateProvider>
         <Component {...pageProps} />
       </GlobalStateProvider>
